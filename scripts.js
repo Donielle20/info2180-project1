7e78e85b4216be3d9fca4958aa3b30608a8ae54b
Donielle20 <donielle.hope@yahoo.com>
2021-10-21 20:42:24 -0500

    Modified initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function()
+{
+    document.querySelector('button').addEventListener('click', function(event) 
+    {
+        event.preventDefault();
+    });
+
+    document.getElementsByTagName("button")[0].addEventListener("click",divA);
+},false);
+
+
+
+function divA()
+{
+    let eml = document.querySelector('input').value;
+
+    if (eml == "")
+    {
+        document.getElementsByTagName("div")[20].textContent = "Please enter a valid email address";
+    }
+    else
+    {
+        document.getElementsByTagName("div")[20].textContent = "Thank you! Your email address ";
+
+        let app = document.getElementsByTagName("div")[20];
+
+        app.append(eml);
+
+        app.append(' has been added to our mailing list.');
+    }
+}
